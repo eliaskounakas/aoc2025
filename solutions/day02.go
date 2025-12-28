f9f8acc5caa2be8733410559d6829df87b118f09
eliaskounakas <elias.kounakas@proton.me>
2025-12-28 10:33:12 +0100

    day 2 done

--- solutions/day02.go
+++ solutions/day02.go
@@ -51,6 +51,67 @@ func solveDay2Part1(input []string) int {
 }
 
 func solveDay2Part2(input []string) int {
+	invalidIDsSum := 0
 
-	return 0
+	for i := range input {
+		ids := strings.Split(input[i], "-")
+		firstID, err := strconv.Atoi(ids[0])
+
+		if err != nil {
+			return 0
+		}
+
+		secondID, err := strconv.Atoi(ids[1])
+
+		if err != nil {
+			return 0
+		}
+
+		for currID := firstID; currID <= secondID; currID++ {
+			if isInvalidID(currID) {
+				invalidIDsSum += currID
+			}
+		}
+	}
+
+	return invalidIDsSum
+}
+
+func isInvalidID(id int) bool {
+	strID := strconv.Itoa(id)
+
+	for subStringSize := 1; subStringSize <= len(strID)/2; subStringSize++ {
+		if subStringSize == 0 || (len(strID)%subStringSize) != 0 {
+			continue
+		}
+
+		subStrings := chunkString(strID, subStringSize)
+
+		invalid := true
+		for _, s := range subStrings {
+			if s != subStrings[0] {
+				invalid = false
+				break
+			}
+		}
+
+		if invalid {
+			fmt.Println(strID)
+			return true
+		}
+	}
+
+	return false
+}
+
+// Splits a string into substrings of size argument.
+// for example: s is a string of length 8, if size is 2,
+// it will get split into 4 strings of length 2
+func chunkString(s string, size int) []string {
+	var chunks []string
+	for i := 0; i < len(s); i += size {
+		end := min(i+size, len(s))
+		chunks = append(chunks, s[i:end])
+	}
+	return chunks
 }
